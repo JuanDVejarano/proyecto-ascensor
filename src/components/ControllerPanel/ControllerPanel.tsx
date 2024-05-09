58b27f56a761dd2ee0845f1ae6e961148db6908c
JuanDVejarano <juanjdv_200@hotmail.com>
2024-05-09 11:13:24 -0500

    Configuracion estructura de componentes

---
+++ src/components/ControllerPanel/ControllerPanel.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './ControllerPanel.scss';
+
+interface ControllerPanelProps {}
+
+const ControllerPanel: FC<ControllerPanelProps> = () => (
+  <div className="ControllerPanel">
+    ControllerPanel Component
+  </div>
+);
+
+export default ControllerPanel;
---
+++ src/components/RequestTable/RequestTable.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './RequestTable.scss';
+
+interface RequestTableProps {}
+
+const RequestTable: FC<RequestTableProps> = () => (
+  <div className="RequestTable">
+    RequestTable Component
+  </div>
+);
+
+export default RequestTable;
